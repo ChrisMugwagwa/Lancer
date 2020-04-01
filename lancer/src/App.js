663cdf68912e09048bb8e57a6d82e73b15f22eb6
Grandeur Vegah <gvega001@gold.ac.uk>
2020-04-01 12:30:35 +0100

    Person junk

--- lancer/src/App.js
+++ lancer/src/App.js
@@ -1,31 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
+//import ReactDOM from 'react-dom';
+import './index.css';
+import Person from './Person/Person'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Header from './component/Header';
 
-function App() {
-  return (
-    
-    <div className="App">
-      <Header></Header>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="game">
+        <h1> Hello</h1>
+        <Person name = "Grandeur" age = "29"/>
+        </div>
+        
+        
+    );
+  }
 }
 
 export default App;
