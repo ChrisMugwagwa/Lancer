a527b12a96bce75ea7317786d794c39d0f645d9b
Christian Mugwagwa <mugwagwachristian@gmail.com>
2020-04-01 20:30:19 +0100

    added a busted jumbotron splash. still to be completed. not responsive

--- lancer/src/App.js
+++ lancer/src/App.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './component/Header';
+import Splash from './component/Splash';
 
 function App() {
   return (
     
     <div className="App">
       <Header></Header>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Splash></Splash>
     </div>
   );
 }
---
+++ lancer/src/component/Splash.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import '../App.css';
+//import Carousel from 'react-bootstrap/Carousel';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+
+
+
+export default class Splash extends Component {
+    render() {
+        return (
+            <Jumbotron className="mainSplash">
+                <div className="titleHeader">
+                <h1>Lancer</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <p>
+                    <Button variant="primary">Learn more</Button>
+                </p>
+                </div>
+            </Jumbotron>
+        );
+    }
+}
