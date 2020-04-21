a683745674abbe9059a80148402b215f2df6b555
Christian Mugwagwa <mugwagwachristian@gmail.com>
2020-04-21 15:38:11 +0100

    added example of the useContext hook and context providers/consumers

--- lancer/src/component/Cart.js
+++ lancer/src/component/Cart.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useMemo } from 'react';
+import AuthContext from '../context/auth-context';
 
 export default function Cart() {
 
@@ -6,25 +7,40 @@ export default function Cart() {
 
     //This function takes an item. uses setCart state to retrun a new list with all the items that arent item (i !== item). so the returned list 
     const [stockState, setStock] = useState(initialItems);
-    const dispense = item => { 
+    const dispense = item => {
         setStock(items => items.filter(i => i !== item))
-     };
+    };
 
-     useEffect(() => {
-         //Didnt know this but using the back quote character ` (usually above tab,left of 1) allows
-         // you to put values in strings similar to python f-strings with the $ character (which is a template literal in ES6)
+    useEffect(() => {
+        //Didnt know this but using the back quote character ` (usually above tab,left of 1) allows
+        // you to put values in strings similar to python f-strings with the $ character (which is a template literal in ES6)
         // so a line like: document.title = "You have: " + stockState.length + " items"; becomes
         document.title = `You have ${stockState.length} items`
 
         //This is the optional return statement of useState which will call the function on a dismount
-        return(() => {
+        return (() => {
             document.title = "Lancer";
         })
-     }, [stockState.length]);
+    }, [stockState.length]);
+
+    function loginStatus() {
+        
+    }
 
     return (
-        <div>
-            <h4>Stock Manager</h4>
+        
+            <div>
+                <h4>Stock Manager</h4>
+                <AuthContext.Consumer>
+                {()=>{
+                    if (AuthContext.authenticated === false) {
+                        return "NOT Logged In : " + AuthContext.authenticated;
+                    } else {
+                        return "Logged In Successfully: " + AuthContext.authenticated;
+                    }
+                }}
+                </AuthContext.Consumer>
+                
                 <div>
                     <h5>Availiable Items:</h5>
                     {stockState.length === 0 ? (
@@ -33,13 +49,13 @@ export default function Cart() {
                             <ul>
                                 {stockState.map(item => (
                                     <li key={item}>
-                                        <button onClick={()=>dispense(item)}>Buy</button> {item}
+                                        <button onClick={() => dispense(item)}>Buy</button> {item}
                                     </li>
                                 ))}
                             </ul>
                         )}
+                </div>
             </div>
-        </div>
     )
 
 }
