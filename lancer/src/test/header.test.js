1ce3876381b41b95d27d94d733a70077d1b0571a
ChrisMugwagwa <mugwagwachristian@gmail.com>
2020-05-04 11:03:35 +0100

    added docker and concourse pipeline files for CI automation. dockerfile contains a node image and nginx image

--- lancer/src/test/header.test.js
+++ lancer/src/test/header.test.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import {render, cleanup} from '@testing-library/react';
+import React, { useContext } from 'react';
+import {render, cleanup, getByTitle} from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import Header from '../component/Header'
+import AuthContext from '../context/auth-context';
 
-afterEach(cleanup);
 
-it ("Displays the header",() =>{
-    const tree = renderer.create(<Header></Header>)
-    expect(tree).toMatchSnapshot();
-});
+describe("Testing the header component", ()=>{
+    
+    afterEach(cleanup);
+//really we shouldnt use a snapshot afterwards for subsequent states becasue the login button changes state
+    it ("Displays the header",() =>{
+        const tree = renderer.create(<Header></Header>)
+        expect(tree).toMatchSnapshot();
+    });
+
+    it ("Displays a logout button if logged in", ()=>{
+        const header = () => {
+            const loginStatus = useContext(AuthContext)
+            return <div>
+                <AuthContext.Provider value={
+                    {
+                    authenticated: true,
+                    login: () => {
+                        setUser({ loggedIn: true })
+                    },
+                    logout: () => {
+                        setUser({ loggedIn: false })
+                    }
+                    }
+                }>
+                    <Header></Header>
+                </AuthContext.Provider>
+            </div>
+        }
+        const {getByTitle} = render(header)
+        expect(getByTitle("loginButton").toHaveTextContent("Login"))
+    });
+
+})
